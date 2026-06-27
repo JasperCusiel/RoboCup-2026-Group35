09f1c25cf2885bdf1050bfc7cd5e6fb2c4acc9f4
Jasper Cusiel <jaspercusiel@icloud.com>
2026-06-28 11:07:26 +1200

    Update to show robot on seperate figure using Visualizer2D class. Remove unused variables.

--- simulation.m
+++ simulation.m
@@ -1,18 +1,6 @@
 clc; clear; close all;
 
-% create and setup slam object 
-slamObj = slam;
-
-setup(slamObj);
-
-% sim robot parameters
-numBeams = 6;
-angles = linspace(-pi, pi, numBeams);
-
-% Start pose
-pose = [1.0, 1.0, 0];
-
-%  Truth Map
+%% Create Truth Map
 res = 10;  % cells per metre
 mapTruth = binaryOccupancyMap(3, 5, res);
 
@@ -26,7 +14,12 @@ grid(10:30, 25) = 1;   % internal vertical line
 grid(30, 10:20) = 1;   % internal horizontal line
 setOccupancy(mapTruth, grid);
 
-% Differential Drive Robot
+%% Create and setup slam object 
+slamObj = slam;
+setup(slamObj);
+
+
+%% Robot Setup
 wheelRadius = 0.1; % [m]   Radius of each wheel
 trackWidth = 0.5; % [m]   Distance between left and right wheels
 maxWheelSpeed = 100*2*pi/60; % [rad/s] Maximum wheel speed (100 RPM converted to rad/s)
@@ -35,20 +28,21 @@ vehicle.WheelSpeedRange = [-1 1]*maxWheelSpeed;
 vehicle.TrackWidth = trackWidth;
 vehicle.WheelRadius = wheelRadius;
 
-% Lidar Sensors
+%% Lidar Sensors
 a = 0.15;                  % robot half-length [m]
 b = 0.15;                  % robot half-width  [m]
 o = 0.05; % sensor offset
 maxRange = 4; % m
 
-sensorOffset = [ a b;     % front-left
-                  a (b-o);    % front-right
-                  a (b -2*o)
-                  a -(b -2*o)
-                  a -(b-o);    % rear-left
-                  a -b];   % rear-right
+% offsets are from robot center
+sensorOffsets = [ a -b;     % front-left
+                  a -(b - o);    % front-right
+                  a -(b - 2*o);
+                  a (b - 2*o);
+                  a (b - o);    % rear-left
+                  a b];   % rear-right
 
-sensorAngle = deg2rad([60, 36, 12, -12, -36, -60]);
+sensorAngles = deg2rad([-30, -20, 10, -10, 20, 30]);
 
 lidar1 = LidarSensor();
 lidar2 = LidarSensor();
@@ -58,37 +52,49 @@ lidar5 = LidarSensor();
 lidar6 = LidarSensor();
 
 
-lidar1.mapName = mapTruth;
-lidar1.sensorOffset = sensorOffset(1);
-lidar1.scanAngles = sensorAngle(1);
+lidar1.mapName = 'mapTruth';
+lidar1.sensorOffset = sensorOffsets(1,:);
+lidar1.scanAngles = sensorAngles(1);
 lidar1.maxRange = maxRange;
 
-lidar2.mapName = mapTruth;
-lidar2.sensorOffset = sensorOffset(2);
-lidar2.scanAngles = sensorAngle(2);
+lidar2.mapName = 'mapTruth';
+lidar2.sensorOffset = sensorOffsets(2,:);
+lidar2.scanAngles = sensorAngles(2);
 lidar2.maxRange = maxRange;
 
-lidar3.mapName = mapTruth;
-lidar3.sensorOffset = sensorOffset(3);
-lidar3.scanAngles = sensorAngle(3);
+lidar3.mapName = 'mapTruth';
+lidar3.sensorOffset = sensorOffsets(3,:);
+lidar3.scanAngles = sensorAngles(3);
 lidar3.maxRange = maxRange;
 
-lidar4.mapName = mapTruth;
-lidar4.sensorOffset = sensorOffset(4);
-lidar4.scanAngles = sensorAngle(4);
+lidar4.mapName = 'mapTruth';
+lidar4.sensorOffset = sensorOffsets(4,:);
+lidar4.scanAngles = sensorAngles(4);
 lidar4.maxRange = maxRange;
 
-lidar5.mapName = mapTruth;
-lidar5.sensorOffset = sensorOffset(5);
-lidar5.scanAngles = sensorAngle(5);
+lidar5.mapName = 'mapTruth';
+lidar5.sensorOffset = sensorOffsets(5,:);
+lidar5.scanAngles = sensorAngles(5);
 lidar5.maxRange = maxRange;
 
-lidar6.mapName = mapTruth;
-lidar6.sensorOffset = sensorOffset(6);
-lidar6.scanAngles = sensorAngle(6);
+lidar6.mapName = 'mapTruth';
+lidar6.sensorOffset = sensorOffsets(6,:);
+lidar6.scanAngles = sensorAngles(6);
 lidar6.maxRange = maxRange;
 
-% Vector Field Histogram
+%% Visualizer
+viz = Visualizer2D;
+viz.mapName = 'mapTruth';
+attachLidarSensor(viz,lidar1);
+attachLidarSensor(viz,lidar2);
+attachLidarSensor(viz,lidar3);
+attachLidarSensor(viz,lidar4);
+attachLidarSensor(viz,lidar5);
+attachLidarSensor(viz,lidar6);
+viz.sensorOffset = sensorOffsets;
+viz.scanAngles = sensorAngles;
+
+%% Vector Field Histogram setup
 VFH = controllerVFH();
 VFH.NumAngularSectors = 36;
 VFH.DistanceLimits = [0.05, 4];
@@ -112,7 +118,7 @@ PP.Waypoints = [1.5 1.5]; % center of field
 % Calculate maximum angular velocity based on vehicle physical limits.
 % For a differential drive vehicle, maximum angular velocity occurs when
 % the wheels rotate in opposite directions at maximum speed.
-maxAngularVelocity = 3;                   % rad/s
+maxAngularVelocity = 200;                   % rad/s
 
 lookaheadTime = 2;                                                             % [seconds]
 PP.LookaheadDistance = 0.8; % [meters]
@@ -121,11 +127,11 @@ PP.LookaheadDistance = 0.8; % [meters]
 % Simulation loop 
 % Define simulation parameters
 sampleTime = 0.1;                        % Time step for simulation [seconds]
-maxSimTime = 30;                         % Maximum simulation time [seconds]
+maxSimTime = 10;                         % Maximum simulation time [seconds]
 simSteps = ceil(maxSimTime/sampleTime);  % Total number of simulation steps
 timeVec = (0:simSteps-1)*sampleTime;
 
-initialPose = [1 1 0];                   % Initial pose: [x y theta]
+initialPose = [1.0; 1.0; 0.0];                   % Initial pose: [x y theta]
 currentPose = initialPose;               % Set current pose to initial pose
 poses = zeros(simSteps, 3);              % Preallocate array to record robot trajectory
 % Initialize arrays to record velocity commands
@@ -139,15 +145,12 @@ reset(PP);
 for idx = 1:simSteps
 
    % Simulate Lidar
-    pts = rayIntersection(mapTruth, currentPose, angles, maxRange);
-    dx = pts(:,1) - currentPose(1);
-    dy = pts(:,2) - currentPose(2);
-    ranges = sqrt(dx.^2 + dy.^2)';
-    ranges(isnan(ranges)) = maxRange;
+    ranges = [lidar1(currentPose), lidar2(currentPose), lidar3(currentPose), lidar4(currentPose), lidar5(currentPose), lidar6(currentPose)];
+
 
     % SLAM + planning
     [map, goalXY, path, pathLength, hasFrontier, mapUpdated, frontierXY] = ...
-        step(slamObj, ranges, angles, currentPose);
+        step(slamObj, ranges, sensorAngles, currentPose);
 
     % Pull out non zero way points
     if (pathLength > 0)
@@ -171,7 +174,7 @@ for idx = 1:simSteps
     
     desiredHeading = atan2(nextPoint(2)-currentPose(2), ...
                            nextPoint(1)-currentPose(1));
-    steeringDirection = VFH(ranges, angles, desiredHeading);
+    steeringDirection = VFH(ranges, sensorAngles, desiredHeading);
 
     headingError = wrapToPi(steeringDirection - currentPose(3));
     wVFH = 2.0 * headingError;
@@ -185,8 +188,8 @@ for idx = 1:simSteps
 
     % update sim pose
     vel = derivative(vehicle,currentPose,[vCmd wCmd]);
-    currentPose = currentPose + vel'*sampleTime;
-    poses(idx,:) = currentPose;
+    currentPose = currentPose(:) + vel * sampleTime;
+    % poses(idx,:) = currentPose;
 
    
     fprintf("Step %d | Frontiers: %d | PathLen: %d | Steering Direction: %d\n", ...
@@ -195,16 +198,17 @@ for idx = 1:simSteps
 
     % Ploting
     if mod(idx,1) == 0
-        figure(1); clf;
+        viz(currentPose, ranges);
+        figure(3); clf;
 
-        subplot(1,2,1)
-        show(mapTruth);
-        hold on;
-        plot(currentPose(1), currentPose(2), 'ro');
-        title('Ground Truth')
+        % subplot(1,2,1)
+        % show(mapTruth);
+        % hold on;
+        % plot(currentPose(1), currentPose(2), 'ro');
+        % title('Ground Truth')
 
-        subplot(1,2,2)
-        axis equal tight;
+        % subplot(1,2,2)
+        % axis equal tight;
         title('SLAM Map')
         show(map);
         hold on;
